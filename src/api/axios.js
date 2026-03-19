280e931523b55cb5bf0399768a28bab1cbdaffec
usmanz-dev <usmanzafar17215@gmail.com>
2026-03-20 00:21:21 +0500

    "cc"

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://smartpos-backend-fb5i.vercel.app/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
